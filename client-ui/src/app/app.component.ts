505f74021eab00da1a03bdbe4dd620432eb63b36
ahmet <ahmet.altun60@gmail.com>
2023-02-10 23:19:37 +0300

    Angular projesi yüklendi ve docker konfigürasyonu yapıldı

---
+++ client-ui/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Test, TestService} from './services/test.service';
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Realtime Todo Application';
+  valueFromBackend$: Observable<Test> = this.testService.getUserById("1");
+
+  constructor(private testService: TestService) {
+  }
+}
